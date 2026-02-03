ebe872db59e508228f6d38923dd9bbad487f9dc3
miyabi <GitHub>
2026-02-03 16:34:44 +0900

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import './App.css'
+import { useSelector,useDispatch } from "react-redux"
+import { addPost, deletePost } from './features/Post';
+import { useState } from 'react';
+
+
+function App() {
+
+  const [name,setName] =useState("");
+  const [content,setContent] =useState("");
+
+
+
+  const postList = useSelector((state) => state.posts.value)
+  console.log(postList);
+
+  const dispatch =useDispatch();
+
+  const handleClick =() => {
+    dispatch(addPost({
+      id: postList.length,
+      name: name,
+      content: content,
+    }))
+  
+    setName("");
+
+    setContent("");
+
+
+  }
+
+
+  return (
+    <>
+    <div className="App">
+      <div className="">
+     <h1>React-Redux 掲示板 </h1>
+      </div>
+     <div className="addPost">
+      <input type="text" placeholder='お名前'onChange={(e) => setName(e.target.value)} value={name}/>
+      <input type="text" placeholder='投稿内容' onChange={(e) => setContent(e.target.value)} value={content}/>
+      <button onClick={handleClick}>投稿</button>
+      <hr />
+    </div>
+    <div className="displayPosts">
+      {postList.map((post) => (
+        <div key={post.id} className='post'>
+          <h1 className='postName'>{post.name}</h1>
+          <h1 className='postContent'>{post.content}</h1>
+          <button onClick={() => dispatch(deletePost({id: post.id}))}>削除</button>
+        </div>
+      ))}
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {configureStore} from "@reduxjs/toolkit"
+import {Provider} from "react-redux"
+import postsReducer from "./features/Post.js"
+
+const store = configureStore({
+  reducer: {
+    posts: postsReducer,
+  }
+})
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Provider store={store}>
+    <App />
+    </Provider>
+  </StrictMode>,
+)
